c500e3df62e01d6ee8569d023851bf2757de1a72
Adyxx <adam.stencl@seznam.cz>
2025-07-15 00:19:23 +0200

    overcomplicating deck-build restrictions & models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,6 @@
 from .card import Card
-from .character import Character
+from .character import Character, Faction, Subtype
+from .character_relationship import CharacterRelationship
 from .deck import Deck
 from .deck_card import DeckCard
 from .match import Match
@@ -9,5 +10,3 @@ from .card_effect_binding import CardEffectBinding
 from .condition import Condition
 from .trigger import Trigger
 from .restriction import Restriction
-from .character_pair_synergy import CharacterPairSynergy
-from .character_relationship import CharacterRelationship
